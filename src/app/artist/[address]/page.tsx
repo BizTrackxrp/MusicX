34542d18f8c97c0629adcd5ba90099f0d56c11c6
BizTrackxrp <aventrallc@proton.me>
2025-12-15 15:02:46 -0800

    Create page.tsx

---
+++ src/app/artist/[address]/page.tsx
@@ -0,0 +1,16 @@
+import ArtistPage from './ArtistPage';
+
+export default function Page() {
+  return <ArtistPage />;
+}
+```
+
+**5. Click "Commit changes"**
+
+---
+
+**6. Then do it again - "Add file" → "Create new file"**
+
+**7. In the filename box, type:**
+```
+artist/[address]/ArtistPage.tsx
